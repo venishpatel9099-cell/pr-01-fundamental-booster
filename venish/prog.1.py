f7e777946628b6c30ed29e85c15ee5aa5f36a9d6
venishpatel9099-cell <venishpatel9099@gmail.com>
2026-09-18 16:54:09 +0530

    Add files via upload

---
+++ venish/prog.1.py
@@ -0,0 +1,19 @@
+print("Welcome to the Interarctive Personal Data Collector!");
+
+name = input("Please enter Your Name: ");
+age = int(input("Please enter Your Age: "));
+height = float(input("Please enter Your Height in meters: "));
+num = int(input("Please enter Your Fav Number: "));
+
+print("Thank you! Here is the information we Collected: ");
+
+print(f"name: {name} (Type: {type(name)}, Memory Address: {id(name)}");
+print(f"name: {age} (Type: {type(age)}, Memory Address: {id(age)}");
+print(f"name: {height} (Type: {type(height)}, Memory Address: {id(height)}");
+print(f"name: {num} (Type: {type(num)}, Memory Address: {id(num)}");
+
+birth = 2026-age ;
+
+print("Your Birth Year is approximately: ",birth,);
+
+print("Thank You for using the Personal Data Collector. GoodBye!");
